f02e7b86a98d0fc1d5dccbbfccb8643e9d44fbd9
Juliano Gouveia <juliano@neosacode.com>
2019-02-06 19:06:14 -0200

    Finish iugu integration

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -175,7 +175,7 @@ class CreatePaymentView(View):
                 'address.district': request.user.district,
             },
             'notification_url': 'https://imovelpericiado.com.br/dashboard/iugu-notification',
-            'return_url': 'https://imovelpericiado.com.br/dashboard/new-analyze',
+            'return_url': 'https://imovelpericiado.com.br/dashboard/new-analyze'
         }
 
         invoice = requests.post(url, json=data, auth=HTTPBasicAuth('6C5DE8AAEE9C4C129B27656EC4BC5A64', '')).json()
